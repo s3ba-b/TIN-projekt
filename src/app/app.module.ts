c86ce796581cfac2f034c3fbd695a75363e8bd93
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2020-12-21 21:14:42 +0100

    Update the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TosComponent } from './tos/tos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PrivacyPolicyComponent,
+    TosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
+      { path: 'tos', component: TosComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
