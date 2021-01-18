770b018cd5512204a5f3cb9237e8f9746e04633e
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-01-18 18:29:56 +0100

    Division into categories #1

---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    availabilityDate: new Date('2019-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/9/58971032001/4316e832c11001a88d095b8406f194ec/samsung-ue65tu7102k,58971032001_3.jpg',
+    description: 'Wciągnij się w obraz z miliardem odcieni kolorów. Wyświetlacz Krystaliczny wypełni obraz Twojego telewizora paletą żywych barw, byś mógł cieszyć się każdym, nawet najdrobniejszym niuansem.',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/samsung-ue65tu7102k.bhtml',
+    category: 'telewizory',
+    name: 'Samsung UE65TU7102K'
+  },
+  {
+    availabilityDate: new Date('2019-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/5/60828643409/8042c7885c808bcb7297613fe6e3f1dc/samsung-qe65q67t-smart-tv-uhd-4k-qled,60828643409_3.jpg',
+    description: 'Doskonały kolor, który zapiera dech, niezależnie od jasności. Technologia Quantum Dot wypełni Twój obraz miliardem odcieni kolorów, które będą doskonałe niezależnie od jasności.',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/samsung-qe65q67t-smart-tv-uhd-4k-qled.bhtml',
+    category: 'telewizory',
+    name: 'Samsung QLED QE65Q67TAU'
+  },
+  {
+    availabilityDate: new Date('2019-01-16'),
+    // tslint:disable-next-line:max-line-length
+    imageUrl: 'https://f00.esfr.pl/foto/4/60828604625/4316e832c11001a88d095b8406f194ec/samsung-ue55tu7102-smart-tv-uhd-4k,60828604625_3.jpg',
+    description: 'Wciągnij się w obraz z miliardem odcieni kolorów. Wyświetlacz Krystaliczny wypełni obraz Twojego telewizora paletą żywych barw, byś mógł cieszyć się każdym, nawet najdrobniejszym niuansem.',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/samsung-ue55tu7102-smart-tv-uhd-4k.bhtml',
+    category: 'telewizory',
+    name: 'Samsung UE55TU7102K'
+  },
+  {
+    availabilityDate: new Date('2019-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/5/61883542665/853f822db974c28d28e62a8a1149e045/lg-oled55cx-tv-oled-uhd-4k,61883542665_3.jpg',
+    description: 'LG OLED55CX3LA oferuje nową jakość multimedialnej rozrywki w domowym zaciszu. Telewizor OLED gwarantuje piękny obraz i dźwięk, a także zaskakuje nowoczesnymi technologiami.',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/lg-oled55cx-tv-oled-uhd-4k.bhtml',
+    category: 'telewizory',
+    name: 'LG OLED55CX3LA'
+  },
+  {
+    availabilityDate: new Date('2019-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/9/71327188137/ea08610ee555fb998e75984be75ec019/lg-oled65bx-tv-oled-uhd-4k-lg,71327188137_3.jpg',
+    description: 'Wybierz telewizor wyposażony w matrycę OLED, by zyskać bezkompromisowy obraz – pełen koloru i zbalansowanego kontrastu. Poznaj model LG OLED65BX3LB!',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/lg-oled65bx-tv-oled-uhd-4k-lg.bhtml',
+    category: 'telewizory',
+    name: 'LG OLED65BX3LB'
+  },
+  {
+    availabilityDate: new Date('2019-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/5/61293844337/e35ba0e6ff93dada41ba028d29de7f4e/xiaomi-mi-led-tv-4s-43,61293844337_3.jpg',
+    description: '64-bitowy czterordzeniowy procesor. Szybsza praca urządzenia.Duża pojemność 1.5 GB RAM + 8 GB HDD dla płynnej obsługi większej liczby aplikacji.',
+    sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/xiaomi-mi-led-tv-4s-43.bhtml',
+    category: 'telewizory',
+    name: 'Xiaomi Mi LED TV 4S 43"'
+  }
+];
