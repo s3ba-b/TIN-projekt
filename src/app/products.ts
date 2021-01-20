c7269bb0e06fd6c3b12dffbed9a4eeef97c34abe
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-01-20 18:14:45 +0100

    More products

--- src/app/products.ts
+++ src/app/products.ts
@@ -17,7 +17,6 @@ export const products = [
   },
   {
     availabilityDate: new Date('2021-01-15'),
-    // tslint:disable-next-line:max-line-length
     imageUrl: 'https://f00.esfr.pl/foto/4/60828604625/4316e832c11001a88d095b8406f194ec/samsung-ue55tu7102-smart-tv-uhd-4k,60828604625_3.jpg',
     description: 'Wciągnij się w obraz z miliardem odcieni kolorów. Wyświetlacz Krystaliczny wypełni obraz Twojego telewizora paletą żywych barw, byś mógł cieszyć się każdym, nawet najdrobniejszym niuansem.',
     sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/samsung-ue55tu7102-smart-tv-uhd-4k.bhtml',
@@ -47,5 +46,167 @@ export const products = [
     sourceUrl: 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe/xiaomi-mi-led-tv-4s-43.bhtml',
     category: 'telewizory',
     name: 'Xiaomi Mi LED TV 4S 43"'
-  }
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/8/35646744737/48ba4a0db734bda94fb957da326c3e54/apple-iphone-xr-256gb-koralowy,35646744737_8.jpg',
+    description: 'Designerski smartfon z ekranem Liquid Retina HD o przekątnej 6,1-cala, superszybkim procesorem, aparatem 12 Mpix oraz rozpoznawaniem twarzy.',
+    sourceUrl: 'https://www.euro.com.pl/telefony-komorkowe/apple-iphone-xr-256gb-koralowy.bhtml',
+    category: 'telefony',
+    name: 'APPLE IPHONE XR 256GB CORAL'
+  },
+  {
+    availabilityDate: new Date('2021-01-18'),
+    imageUrl: 'https://f01.esfr.pl/foto/1/35646798265/fb1591a02c258aa65d07d7bf91d3afaf/apple-iphone-xr-256gb-product-red,35646798265_8.jpg',
+    description: 'Designerski smartfon z ekranem Liquid Retina HD o przekątnej 6,1-cala, superszybkim procesorem, aparatem 12 Mpix oraz rozpoznawaniem twarzy.',
+    sourceUrl: 'https://www.euro.com.pl/telefony-komorkowe/apple-iphone-xr-256gb-product-red.bhtml',
+    category: 'telefony',
+    name: 'Apple iPhone Xr 256GB (product red)'
+  },
+  {
+    availabilityDate: new Date('2021-01-23'),
+    imageUrl: 'https://f00.esfr.pl/foto/2/51760198921/8abee5d0844fd433c9b5017da0690e17/samsung-smartfon-galaxy-fold-silver-samsung,51760198921_8.jpg',
+    description: 'Główny wyświetlacz ma przekątną 7,3 cali i rozdzielczość QXGA+, jest wykonany w technologii Dynamic AMOLED. Wyświetlacz zewnętrzny Super AMOLED ma 4,6 cala i rozdzielczość HD+.',
+    sourceUrl: 'https://www.euro.com.pl/telefony-komorkowe/samsung-smartfon-galaxy-fold-silver-samsung.bhtml',
+    category: 'telefony',
+    name: 'Samsung Galaxy FOLD (srebrny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/2/36289061801/947f8bb4f67134e0607080a2d65ba827/honor-smartfon-honor-8x-4-128-black,36289061801_8.jpg',
+    description: 'Pełny ekran HD z zaawansowaną technologią chip-on-film (COF) i specjalną konstrukcją anteny. Spód o szerokości jedynie 4,25 mm i niesamowity stosunek powierzchni ekranu do obudowy: 91%. Wyświetlacz zajmuje prawie cały przód telefonu',
+    sourceUrl: 'https://www.euro.com.pl/telefony-komorkowe/honor-smartfon-honor-8x-4-128-black.bhtml',
+    category: 'telefony',
+    name: 'Honor 8X 128GB (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-11'),
+    imageUrl: 'https://f00.esfr.pl/foto/6/35015026953/40118a5abb2744411a6f1dd554bb112b/pocophone-f1-6-64-gb-grafitowy,35015026953_3.jpg',
+    description: 'Wydajny ośmiordzeniowy procesor Snapdragon 845 i 6 GB pamięci RAM zapewnia wysoką kulturę pracy i brak jakichkolwiek przycięć.',
+    sourceUrl: 'https://www.euro.com.pl/telefony-komorkowe/pocophone-f1-6-64-gb-grafitowy.bhtml',
+    category: 'telefony',
+    name: 'POCOPHONE F1 6/64 GB (grafitowy)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/3/12674430610/e6dcbb2dc67d29fd8a08bab478f9611a/panasonic-hc-vx980-czarny,12674430610_8.jpg',
+    description: 'Rozdzielczość Full HD (1080p), która pozwala na uzyskanie maksymalnej szczegółowości i wierne odwzorowanie kolorów. Zagwarantuje Ci niezwykle wyrazisty obraz.',
+    sourceUrl: 'https://www.euro.com.pl/kamery/panasonic-hc-vx980-czarny.bhtml',
+    category: 'Kamery cyfrowe',
+    name: 'Panasonic HC-VX980 (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/2/8985681452/aa68ce8ac67b69b41aff750475c58a7a/sony-hdr-cx405b-czarny,8985681452_8.jpg',
+    description: 'Zaawansowany stabilizator obrazu umożliwia nagrywanie wyraźnego obrazu nawet w niesprzyjających warunkach. Skutecznie kompensuje drgania ręki użytkownika',
+    sourceUrl: 'https://www.euro.com.pl/kamery/sony-hdr-cx405b-czarny.bhtml',
+    category: 'Kamery cyfrowe',
+    name: 'Sony HDR-CX405B (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/9/9277082412/7b16ced48c8e162f9833b3878ba611fa/sony-fdr-ax33-4k,9277082412_8.jpg',
+    description: 'Nagrywanie obrazu 4K Ultra HD pozwala utrwalić cztery razy więcej szczegółów niż w formacie Full HD Stabilizator obrazu Balanced Optical SteadyShot minimalizuje skutki drżenia ręki trzymającej kamerę.',
+    sourceUrl: 'https://www.euro.com.pl/kamery/sony-fdr-ax33-4k.bhtml',
+    category: 'Kamery cyfrowe',
+    name: 'Sony FDR-AX33 4K'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/3/12847018235/d3fe0c9d1a44df32d640f89899842c77/panasonic-hc-v180-czarny,12847018235_8.jpg',
+    description: ' System HYBRID O.I.S.+ wykorzystuje 5-osiową korekcję, aby dokładnie niwelować rozmycia spowodowane przez niepożądane potrząsanie, bez względu na to, czy filmowanie jest realizowane przy szerokim kącie widzenia czy przy dużym zakresie zoomu.',
+    sourceUrl: 'https://www.euro.com.pl/kamery/panasonic-hc-v180-czarny.bhtml',
+    category: 'Kamery cyfrowe',
+    name: 'Panasonic HC-V180 (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/1/9016598462/312b7d542f0d3ede2c8f2f16ff7f2376/panasonic-hc-v770-czarny,9016598462_8.jpg',
+    description: 'Układ obiektywu napędza każdą z czterech grup soczewek oddzielnie, ograniczając zakresy napędu, co pozwala uzyskać wysoką jakość obrazu, dużą moc powiększania i kompaktową obudowę kamery.',
+    sourceUrl: 'https://www.euro.com.pl/kamery/panasonic-hc-v770-czarny.bhtml',
+    category: 'Kamery cyfrowe',
+    name: 'Panasonic HC-V770 (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/8/72525212449/98d8ebad2cc618e69030a468d1a64649/dji-dron-mavic-mini-2-fly-more-combo-dji,72525212449_8.jpg',
+    description: 'DJI Mini 2 jest przyjazny dla nowych użytkowników, potężny, a jednocześnie kompaktowy. Imponujące działanie, zachwycająca jakość obrazu oraz kreatywne nagrania są w zasięgu zaledwie kilku stuknięć. Odkryj całkowicie nową perspektywę, uwieczniając momenty, które tworzą Twój świat.',
+    sourceUrl: 'https://www.euro.com.pl/drony/dji-dron-mavic-mini-2-fly-more-combo-dji.bhtml',
+    category: 'Drony',
+    name: 'DJI Mini 2 Fly More Combo (Mavic Mini 2 Fly More Combo)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/8/26725220721/370bc26e0a16b46c4855221786148ced/dji-tello-bialy,26725220721_8.jpg',
+    description: 'Tello wyposażony jest w wysokiej jakości procesor obrazu, dzięki czemu wykonuje niesamowite zdjęcia i filmy. Nawet jeśli latanie nie jest Twoją mocną stroną, wykorzystując EZ Shots, będziesz w stanie wykonywać profesjonalnie nagrania i udostępniać je w mediach społecznościowych ze swojego smartfona.',
+    sourceUrl: 'https://www.euro.com.pl/drony/dji-tello-bialy.bhtml',
+    category: 'Drony',
+    name: 'DJI Ryze Tello (biały)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/5/51914191745/59a23e95dd2b61a2d7be57f363716cba/dji-dron-mavic-mini-dji,51914191745_8.jpg',
+    description: 'Kompaktowy i potężny - DJI Mavic Mini to doskonałe narzędzie do uwieczniania wydarzeń w wyjątkowy sposób. Korzystaj z aplikacji DJI Fly i ciesz się z łatwości latania.',
+    sourceUrl: 'https://www.euro.com.pl/drony/dji-dron-mavic-mini-dji.bhtml',
+    category: 'Drony',
+    name: 'DJI Mavic Mini'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/1/72525212049/98d8ebad2cc618e69030a468d1a64649/dji-dron-mavic-mini-2-dji,72525212049_8.jpg',
+    description: 'DJI Mini 2 jest przyjazny dla nowych użytkowników, potężny, a jednocześnie kompaktowy. Imponujące działanie, zachwycająca jakość obrazu oraz kreatywne nagrania są w zasięgu zaledwie kilku stuknięć. Odkryj całkowicie nową perspektywę, uwieczniając momenty, które tworzą Twój świat.',
+    sourceUrl: 'https://www.euro.com.pl/drony/dji-dron-mavic-mini-2-dji.bhtml',
+    category: 'Drony',
+    name: 'DJI Mini 2 (Mavic Mini 2)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/4/62982711209/50a3a5a7b2c6deb2e34dc93cd133c42a/dji-dron-mavic-air-2-fly-more-combo-dji,62982711209_8.jpg',
+    description: 'Zaawansowany dron DJI Mavic Air 2 z rozbudowanym zestawem akcesoriów do fotografii podniebnej i dodatkowymi akumulatorami.',
+    sourceUrl: 'https://www.euro.com.pl/drony/dji-dron-mavic-air-2-fly-more-combo-dji.bhtml',
+    category: 'Drony',
+    name: 'DJI Mavic Air 2 Fly More Combo'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/4/52434160481/8e18afc0b5b74a613a16f19088c1100/apple-airpods-pro-mwp22zm-a,52434160481_8.jpg',
+    description: 'Innowacyjne słuchawki Apple AirPods Pro zapewnią Ci ponadprzeciętne doznania muzyczne w każdej sytuacji. O Twój komfort słuchania dbają aktywna redukcja szumów, superszybkie ładowanie oraz automatyczne łączenie z urządzeniem, z którego będziesz odtwarzać dźwięki.',
+    sourceUrl: 'https://www.euro.com.pl/sluchawki/apple-airpods-pro-mwp22zm-a.bhtml',
+    category: 'Słuchawki bezprzewodowe douszne',
+    name: 'Apple AirPods Pro'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/4/70886916145/65cb162900a52f0eeba58868a1ae19b8/xiaomi-sluch-mi-true-wireless-earbuds-basic-2,70886916145_8.jpg',
+    description: 'Xiaomi Mi True Wireless Earbuds Basic 2 to nowoczesne, w pełni bezprzewodowe słuchawki, dzięki którym możesz cieszyć się najwyższym komfortem rozmów telefonicznych, słuchania muzyki i całej gamy innych rozrywek.',
+    sourceUrl: 'https://www.euro.com.pl/sluchawki/xiaomi-sluch-mi-true-wireless-earbuds-basic-2.bhtml',
+    category: 'Słuchawki bezprzewodowe douszne',
+    name: 'Xiaomi Mi True Wireless Earbuds Basic 2'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/9/77443397609/18039ff03ff04a47b4e2814f9a8fc1f6/samsung-galaxy-buds-pro-sm-r190-czarny,77443397609_8.jpg',
+    description: 'Teraz możesz rozmawiać z drugą osobą nawet, gdy słuchasz muzyki. Kiedy Galaxy Buds Pro wykryją, że zaczynasz prowadzić rozmowę, automatycznie przełączają się w tryb konwersacji. Słuchawki wyłączają ANC i włączają tryb Dźwięków otoczenia, umożliwiając prowadzenie swobodnej rozmowy i sprawiając, że głos rozmówcy jest bardziej wyraźny.',
+    sourceUrl: 'https://www.euro.com.pl/sluchawki/samsung-galaxy-buds-pro-sm-r190-czarny.bhtml',
+    category: 'Słuchawki bezprzewodowe douszne',
+    name: 'Samsung Galaxy Buds Pro SM-R190 (czarny)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f01.esfr.pl/foto/4/77443272641/3f6ee188a955e7d64f1b2ef4384344e6/samsung-galaxy-buds-pro-sm-r190-fioletowy,77443272641_8.jpg',
+    description: 'Zredukuj szum otoczenia nawet o 99%. Galaxy Buds Pro to słuchawki dokanałowe, które nie tylko fizycznie izolują Cię od zewnętrznego hałasu (pasywna redukcja szumów), ale też minimalizują go dzięki aktywnej redukcji szumów (ANC).',
+    sourceUrl: 'https://www.euro.com.pl/sluchawki/samsung-galaxy-buds-pro-sm-r190-fioletowy.bhtml',
+    category: 'Słuchawki bezprzewodowe douszne',
+    name: 'Samsung Galaxy Buds Pro SM-R190 (fioletowy)'
+  },
+  {
+    availabilityDate: new Date('2021-01-16'),
+    imageUrl: 'https://f00.esfr.pl/foto/3/46311471809/44232aa4a1a8b63f40edfdc0b48ea0b2/jabra-elite-active-65t-titanium-black,46311471809_8.jpg',
+    description: 'Dzięki technologii Bluetooth nie będziesz musiał martwić się o splątane kable! Słuchawki są absolutnie bezprzewodowe – wystarczy, że podłączysz zdalnie swój telefon albo komputer, aby cieszyć się muzyką!',
+    sourceUrl: 'https://www.euro.com.pl/sluchawki/jabra-elite-active-65t-titanium-black.bhtml',
+    category: 'Słuchawki bezprzewodowe douszne',
+    name: 'Jabra Elite Active 65t (titanium black)'
+  },
+
+
 ];
